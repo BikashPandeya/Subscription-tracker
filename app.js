f5f3ae7bc6c458a48c27d94fb06f6f7664e31f95
BikashPandeya <bikashpandeya5@gmail.com>
2025-03-03 08:08:12 +0545

    Environment variables

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Heyy whats up");
+import express from "express"
+import { PORT } from "./config/env"
+
+const app = express()
+
+app.get("/" , (req , res) => {
+    res.send("Welcome to Subscription Tracker app")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+})
