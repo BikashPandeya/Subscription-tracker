31fe382790fa963c9dde7881bcd6d2ca6c2c232e
BikashPandeya <bikashpandeya5@gmail.com>
2025-03-06 20:10:20 +0545

    Users details

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 // GET /users -> get all users
 //GET /users/:id -> get a user by id
 
-userRouter.get("/" , (req , res) => {title : "Get all users"})
-userRouter.get("/:id" , (req , res) => {title : "Get users details"})
+userRouter.get("/" ,  getUsers)
+userRouter.get("/:id" ,  getUser)
 userRouter.post("/" , (req , res) => {title : "CREATE new user"})
 userRouter.put("/:id" , (req , res) => {title : "UPDATE user"})
 userRouter.get("/:id" , (req , res) => {title : "DELETE user"})
