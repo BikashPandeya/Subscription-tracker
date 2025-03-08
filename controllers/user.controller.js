3ac32b831593378a20ed0a9a00f2aa86f46d4559
BikashPandeya <bikashpandeya5@gmail.com>
2025-03-08 20:41:32 +0545

    final touches

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,17 +1,23 @@
-
 import User from "../models/user.model.js";
 
-export const getUsers = async(req , res) => {
+export const getUsers = async(req, res, next) => {
     try {
-        const users = await User.find();
+        // Get only the authenticated user's data
+        const user = await User.findById(req.user._id).select("-password");
+
+        if (!user) {
+            const error = new Error("User not found");
+            error.statusCode = 404;
+            throw error;
+        }
 
         res.status(200).json({
-            success : true,
-            message : "Users fetched successfully",
-            data : users
-        })
+            success: true,
+            message: "User fetched successfully",
+            data: user
+        });
     } catch (error) {
-        next(error)
+        next(error);
     }
 }
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,7 +8,7 @@ const userRouter = Router();
 // GET /users -> get all users
 //GET /users/:id -> get a user by id
 
-userRouter.get("/" ,  getUsers)
+userRouter.get("/" ,authorize , getUsers)
 userRouter.get("/:id" , authorize ,  getUser)
 userRouter.post("/" , (req , res) => {title : "CREATE new user"})
 userRouter.put("/:id" , (req , res) => {title : "UPDATE user"})
